007f3890d9c3cbe6dace023b66b951a4a6fc28d0
Sven Ehrke <sven.ehrke@canoo.com>
2012-10-29 12:01:44 +0100

    refactored tests

--- unittest.js
+++ unittest.js
@@ -15,53 +15,63 @@ function assertEquals(expected, actual) {
 	assert.equal(expected, actual, "expected '" + expected + "' but got '" + actual + "'");
 }
 
+function postFoo() {
+	jsonClient.post('/foo', { "hello": "world", "n1": "v1", "nested": [{"name": "sven"}] }, function(err, req, res, obj) {
+		assert.ifError(err);
+		console.log('%d -> %j', res.statusCode, res.headers);
+		console.log('%j', obj);
+	});
+}
+function postFoo2() {
+	jsonClient.post('/foo2', {"hello": {"name1": "sven"}}, function(err, req, res, obj) {
+		assert.ifError(err);
+		console.log('%d -> %j', res.statusCode, res.headers);
+		console.log('%j', obj);
+	});
 
-console.log("--- cleaning");
-client.del('/delete/all', function(err, req, res, data) {
-	assertEquals('delete called', data);
-	console.log('%s', data);
+}
+function callClean() {
+	console.log("--- cleaning");
+	client.del('/delete/all', function(err, req, res, data) {
+		assertEquals('delete called', data);
+		console.log('%s', data);
 
+		callGetSven1();
+	});
+}
+
+function callGetSven1() {
 	console.log("--- get");
 	client.get('/text/sven', function(err, req, res, data) {
 		assertEquals('error=sven not found in DB', data);
 		console.log('%s', data);
 
-
-		console.log("--- save");
-		client.get('/save/sven?txt=text1', /*{key: 'sven', txt: 'text1'},*/ function(err, req, res, data) {
-//		client.post('/save', {key: 'sven', txt: 'text1'}, function(err, req, res, data) {
-//			assert.ifError(err);
-  			console.log('%d -> %j', res.statusCode, res.headers);
-  			console.log('%s', data);
-
-  			assertEquals('saved sven=text1', data);
-			console.log('%s', data);
-
-			console.log("--- get");
-			client.get('/text/sven', function(err, req, res, data) {
-				assertEquals('text=text1', data);
-				console.log('%s', data);
-
-				console.log('*** TEST DONE ***');
-
-//					client.post('/foo2', '"hello": "world", "n1": "v1", "nested": {"name": "sven"}', function(err, req, res, obj) {
-					jsonClient.post('/foo2', {"hello": {"name1": "sven"}}, function(err, req, res, obj) {
-						assert.ifError(err);
-						console.log('%d -> %j', res.statusCode, res.headers);
-						console.log('%j', obj);
-					});
-
-/*
-				jsonClient.post('/foo', { "hello": "world", "n1": "v1", "nested": [{"name": "sven"}] }, function(err, req, res, obj) {
-					assert.ifError(err);
-					console.log('%d -> %j', res.statusCode, res.headers);
-					console.log('%j', obj);
-				});
-*/
-			});
-
-		});
+		callSaveSven();
 
 	});
-});
+}
+function callGetSven2() {
+	console.log("--- get");
+	client.get('/text/sven', function(err, req, res, data) {
+		assertEquals('text=text1', data);
+		console.log('%s', data);
+
+		console.log('*** TEST DONE ***');
+			postFoo2();
+			//postFoo();
+	});
+}
+
+function callSaveSven() {
+	console.log("--- save");
+	client.get('/save/sven?txt=text1', /*{key: 'sven', txt: 'text1'},*/ function(err, req, res, data) {
+			console.log('%d -> %j', res.statusCode, res.headers);
+			console.log('%s', data);
+			assertEquals('saved sven=text1', data);
+		console.log('%s', data);
+		callGetSven2();
+	});
+}
+
+callClean();
 
